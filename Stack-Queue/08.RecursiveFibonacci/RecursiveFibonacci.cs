49696e4ad4b69c59028a5d90558a2d7d5b580f82
Galya Georgieva <alice.gospodinova@gmail.com>
2016-05-20 14:02:06 +0300

    Recursive Fibonacci

---
+++ Stack-Queue/08.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.RecursiveFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    class RecursiveFibonacci
+    {
+        private static Dictionary<int, long> fibDictionary = new Dictionary<int, long>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long fib = GetFibonacci(n);
+            Console.WriteLine(fib);
+        }
+
+        public static long GetFibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+            
+            if (fibDictionary.ContainsKey(n))
+            {
+                return fibDictionary[n];
+            }
+
+            long current = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            fibDictionary.Add(n, current);
+            return current;
+        }
+    }
+}
