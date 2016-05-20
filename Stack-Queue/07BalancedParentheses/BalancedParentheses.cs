918fc84fad421d23c5ca77caa15f28ee0d42486d
Galya Georgieva <alice.gospodinova@gmail.com>
2016-05-20 12:42:11 +0300

    Balanced Parentheses

---
+++ Stack-Queue/07BalancedParentheses/BalancedParentheses.cs
@@ -0,0 +1,67 @@
+﻿namespace _07BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class BalancedParentheses
+    {
+        static void Main()
+        {
+            char[] parentheses = Console.ReadLine().ToCharArray();
+            string result;
+            char[] openParentheses = { '[', '{', '(' };
+            char[] closeParentheses = { ']', '}', ')' };
+            bool allSame = parentheses.All(item => item == 32);
+
+            if (parentheses.Length % 2 != 0 || parentheses.Length == 0 || allSame)
+            {
+                result = "NO";
+            }
+            else
+            {
+                Queue<char> queue = new Queue<char>();
+                Stack<char> stack = new Stack<char>();
+
+                for (int i = 0; i < parentheses.Length; i++)
+                {
+                    if (openParentheses.Contains(parentheses[i]))
+                    {
+                        queue.Enqueue(parentheses[i]);
+                    }
+                    else if (closeParentheses.Contains(parentheses[i]))
+                    {
+                        stack.Push(parentheses[i]);
+                    }
+                    else if (parentheses[1] == ' ' && (i % 2 == 0))
+                    {
+                        queue.Enqueue(' ');
+                    }
+                    else if (parentheses[1] == ' ' && (i % 2 != 0))
+                    {
+                        stack.Push(' ');
+                    }
+                }
+
+                bool isEqual = true;
+                
+                for (int i = 0; i < queue.Count; i++)
+                    {
+                        char left = queue.Dequeue();
+                        char right = stack.Pop();
+                        bool leftEqualToRignt = ((left == '[' && right == ']') || (left == '{' && right == '}') || (left == '(' && right == ')') || left == right);
+
+                        if (!leftEqualToRignt)
+                        {
+                            isEqual = false;
+                            break;
+                        }
+                    }
+                
+                result = isEqual ? "YES" : "NO";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
