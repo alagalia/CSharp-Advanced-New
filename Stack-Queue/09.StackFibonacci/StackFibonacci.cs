3179fa72db881a36905f62df717359277eda258b
Galya Georgieva <alice.gospodinova@gmail.com>
2016-05-21 20:11:06 +0300

    Use Stack for calculate Fibonacci

---
+++ Stack-Queue/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace _09.StackFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    class StackFibonacci
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<long> fib = new Stack<long>();
+            fib.Push(1);
+            fib.Push(1);
+            long resultFib = 1;
+            for (int i = 0; i < n - 2; i++)
+            {
+                long secondNum = fib.Pop();
+                long firstNum = fib.Pop();
+                resultFib = firstNum + secondNum;
+                fib.Push(secondNum);
+                fib.Push(resultFib);
+            }
+
+            Console.WriteLine(resultFib);
+        }
+    }
+}
