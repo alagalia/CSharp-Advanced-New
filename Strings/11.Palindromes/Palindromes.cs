3ec5dcea8add0335692a3a52c6efcdfa9f5a1ce5
Galya Georgieva <alice.gospodinova@gmail.com>
2016-06-03 13:00:37 +0300

    Palindromes

---
+++ Strings/11.Palindromes/Palindromes.cs
@@ -0,0 +1,39 @@
+﻿
+namespace _11.Palindromes
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Text.RegularExpressions;
+
+    public class Palindromes
+    {
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static void Main()
+        {
+            string line = Console.ReadLine();
+            string[] words = Regex.Split(line, @"\W+");
+
+            List<string> sortedOutput = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                string reversed = Reverse(words[i]);
+                bool isnotEmpty = words[i] != String.Empty;
+                if (isnotEmpty && words[i] == reversed)
+                {
+                    sortedOutput.Add(words[i]);
+                }
+            }
+
+            sortedOutput.Sort();
+            string result = "[" + string.Join(", ", sortedOutput) + "]";
+            Console.WriteLine(result);
+        }
+    }
+}
