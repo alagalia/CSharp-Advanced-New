f0b7a571502c2c104d1e247525be551236679e4b
Galya Georgieva <alice.gospodinova@gmail.com>
2016-06-06 14:43:10 +0300

    Exercises 02 and 03 form homework

---
+++ Regex/RegexExercises/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.MatchPhoneNumber
+{
+    using System.Text.RegularExpressions;
+
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            string pattern = @"\+359([\s|-])2\1\d{3}\1\d{4}\b";
+
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                bool ismatch = regex.IsMatch(input);
+                if (ismatch)
+                {
+                    Console.WriteLine(input.Trim());
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Regex/RegexExercises/03.SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.SeriesOfLetters
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class SeriesOfLetters
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(.)\1*";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Groups[1].Value);
+            }
+
+        }
+    }
+}
